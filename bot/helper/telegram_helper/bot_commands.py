ffe05a8a678a02ff0b813c5da509d4e933c34815
pikachu669 <tusharborde0143@gmail.com>
2024-03-19 14:17:11 +0000

    Replace bot_commands.py

--- bot/helper/telegram_helper/bot_commands.py
+++ bot/helper/telegram_helper/bot_commands.py
@@ -12,7 +12,7 @@ class _BotCommands:
         self.QbLeechCommand     = [f'qbleech{CMD_SUFFIX}',   f'qbl{CMD_SUFFIX}']
         self.YtdlLeechCommand   = [f'ytdlleech{CMD_SUFFIX}', f'ytl{CMD_SUFFIX}']
         self.CancelAllCommand   = [f'cancelall{CMD_SUFFIX}', 'cancelallbot']
-        self.RestartCommand     = [f'restart{CMD_SUFFIX}',  f'r{CMD_SUFFIX}','restartall']
+        self.RestartCommand     = [f'restart{CMD_SUFFIX}',   'restartall']
         self.StatusCommand      = [f'status{CMD_SUFFIX}',    'sall']
         self.PingCommand        = [f'ping{CMD_SUFFIX}',      'p']
         self.StatsCommand       = [f'stats{CMD_SUFFIX}',     's']
@@ -20,11 +20,11 @@ class _BotCommands:
         self.CountCommand       = f'count{CMD_SUFFIX}'
         self.DeleteCommand      = f'del{CMD_SUFFIX}'
         self.CancelMirror       = f'abort{CMD_SUFFIX}'
-        self.ListCommand        = [f'list{CMD_SUFFIX}', f'li{CMD_SUFFIX}']
+        self.ListCommand        = f'list{CMD_SUFFIX}'
         self.SearchCommand      = f'search{CMD_SUFFIX}'
         self.UsersCommand       = f'users{CMD_SUFFIX}'
         self.AuthorizeCommand   = f'authorize{CMD_SUFFIX}'
-        self.UnAuthorizeCommand = [f'unauthorize{CMD_SUFFIX}', f'ua{CMD_SUFFIX}]
+        self.UnAuthorizeCommand = f'unauthorize{CMD_SUFFIX}'
         self.AddSudoCommand     = f'addsudo{CMD_SUFFIX}'
         self.RmSudoCommand      = f'rmsudo{CMD_SUFFIX}'
         self.HelpCommand        = f'help{CMD_SUFFIX}'
@@ -33,8 +33,8 @@ class _BotCommands:
         self.EvalCommand        = f'eval{CMD_SUFFIX}'
         self.ExecCommand        = f'exec{CMD_SUFFIX}'
         self.ClearLocalsCommand = f'clearlocals{CMD_SUFFIX}'
-        self.BotSetCommand      = [f'bsetting{CMD_SUFFIX}', f'bs{CMD_SUFFIX}]
-        self.UserSetCommand     = [f'usetting{CMD_SUFFIX}', f'us{CMD_SUFFIX}'] 
+        self.BotSetCommand      = f'bsetting{CMD_SUFFIX}'
+        self.UserSetCommand     = f'usetting{CMD_SUFFIX}'
         self.BtSelectCommand    = f'btsel{CMD_SUFFIX}'
         self.RssCommand         = f'rss{CMD_SUFFIX}'
         self.CategorySelect     = f'catsel{CMD_SUFFIX}'
